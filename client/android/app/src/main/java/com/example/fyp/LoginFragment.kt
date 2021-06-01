4eff1f2b7d455c1b259db7983156814d3d57ff4e
Arvin Ng <arvin07143@gmail.com>
2021-06-01 19:22:04 +0800

    fixed login flow

--- client/android/app/src/main/java/com/example/fyp/LoginFragment.kt
+++ client/android/app/src/main/java/com/example/fyp/LoginFragment.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.fyp.databinding.FragmentLoginBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -24,8 +28,9 @@ import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
-    private lateinit var auth:FirebaseAuth
+    private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var binding:FragmentLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,28 +41,33 @@ class LoginFragment : Fragment() {
                 .requestIdToken(getString(R.string.default_web_client_id)).build()
         googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
 
-        Log.e("a","FRAGMENT CREATE")
+        Log.e("a", "FRAGMENT CREATE")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkUserInformation()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        Log.e("a","FRAGMENT CREATE")
-        val binding = FragmentLoginBinding.inflate(layoutInflater)
+        Log.e("a", "FRAGMENT CREATE")
+        binding = FragmentLoginBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
         binding.btnSignInWithEmail.setOnClickListener {
             if (validateEmail(binding.emailField)) {
                 auth.signInWithEmailAndPassword(
                     binding.emailField.editText?.text.toString(),
                     binding.passwordField.editText?.text.toString()
-                ).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        Log.d(LoginFragment.TAG, "signInWithEmail:success")
-                        findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
+                ).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "signInWithEmail:success")
+                        checkUserInformation()
                     } else {
                         // If sign in fails, display a message to the user.
-                        Log.w(LoginFragment.TAG, "signInWithEmail:failure", it.exception)
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
                         Toast.makeText(
                             this.context, "Authentication failed.",
                             Toast.LENGTH_SHORT
@@ -66,6 +76,11 @@ class LoginFragment : Fragment() {
                 }
             }
         }
+
+        binding.txtRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_register)
+        }
+
         binding.btnGoogleSignIn.setOnClickListener {
             signInWithGoogle()
         }
@@ -81,11 +96,11 @@ class LoginFragment : Fragment() {
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)!!
-                Log.d(LoginFragment.TAG, "firebaseAuthWithGoogle:" + account.id)
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
                 firebaseAuthWithGoogle(account.idToken!!)
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
-                Log.w(LoginFragment.TAG, "Google sign in failed", e)
+                Log.w(TAG, "Google sign in failed", e)
             }
         }
     }
@@ -96,11 +111,12 @@ class LoginFragment : Fragment() {
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    Log.d(LoginFragment.TAG, "signInWithCredential:success")
+                    checkUserInformation()
+                    Log.d(TAG, "signInWithCredential:success")
                     val user = auth.currentUser
                 } else {
                     // If sign in fails, display a message to the user.
-                    Log.w(LoginFragment.TAG, "signInWithCredential:failure", task.exception)
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
                 }
             }
     }
@@ -121,9 +137,43 @@ class LoginFragment : Fragment() {
         }
     }
 
+    private fun checkUserInformation() {
+        binding.progressCard.visibility = View.VISIBLE
+        var idToken = ""
+        auth.currentUser!!.getIdToken(true).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                idToken = task.result?.token.toString()
+                val requestQueue = Volley.newRequestQueue(requireContext())
+                val getDataURL = "http://10.0.2.2:5000/user/retrieve/"
+                val checkUserProfileRequest = object : JsonObjectRequest(
+                    Method.GET, getDataURL, null,
+                    Response.Listener {
+                        if (it != null) {
+                            findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
+                        }
+
+                    }, {
+                        Log.e("ERROR", it.toString())
+                        findNavController().navigate(R.id.action_loginFragment_to_registerInformationFragment)
+                    }) {
+                    @Throws(AuthFailureError::class)
+                    override fun getHeaders(): Map<String, String> {
+                        val headers = HashMap<String, String>()
+                        headers["Content-Type"] = "application/json"
+                        headers["Authorization"] = idToken
+                        Log.e("AUTH", headers["Authorization"]!!)
+                        return headers
+                    }
+                }
+                requestQueue.add(checkUserProfileRequest)
+            }
+        }
+    }
+
     companion object {
         private const val TAG = "LoginFragment"
         private const val RC_SIGN_IN = 9001
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             LoginFragment().apply {
--- client/android/app/src/main/java/com/example/fyp/RegisterInformationFragment.kt
+++ client/android/app/src/main/java/com/example/fyp/RegisterInformationFragment.kt
@@ -1,14 +1,15 @@
 package com.example.fyp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.android.volley.AuthFailureError
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.fyp.databinding.FragmentRegisterInformationBinding
@@ -16,15 +17,20 @@ import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.firebase.auth.FirebaseAuth
 import com.kevalpatel2106.rulerpicker.RulerValuePickerListener
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 class RegisterInformationFragment : Fragment() {
 
-    private lateinit var auth : FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
     }
+
+    @SuppressLint("SimpleDateFormat")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,61 +48,76 @@ class RegisterInformationFragment : Fragment() {
             }
         }
 
-        binding.btnRegisterFinish.setOnClickListener {  }
+        binding.btnRegisterFinish.setOnClickListener { }
 
         binding.heightPicker.selectValue(160)
         binding.weightPicker.selectValue(50)
 
-        binding.textDob.setOnClickListener {
-            val datePicker = MaterialDatePicker.Builder.datePicker().setTitleText("Select Date").build()
-            datePicker.show(parentFragmentManager,datePicker.toString())
+        binding.textDob.editText?.setOnClickListener {
+            val datePicker =
+                MaterialDatePicker.Builder.datePicker().setTitleText("Select Date").build()
+            datePicker.show(parentFragmentManager, datePicker.toString())
+            datePicker.addOnPositiveButtonClickListener {
+                // Create calendar object and set the date to be that returned from selection
+                val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+                calendar.time = Date(it)
+                val sdf = SimpleDateFormat.getDateInstance()
+                binding.textDob.editText!!.setText(sdf.format(calendar.time))
+            }
         }
 
-        binding.heightPicker.setValuePickerListener(object : RulerValuePickerListener{
+        binding.heightPicker.setValuePickerListener(object : RulerValuePickerListener {
             override fun onValueChange(selectedValue: Int) {
             }
 
             override fun onIntermediateValueChange(selectedValue: Int) {
-                binding.heightIndicator.text = getString(R.string.height_value,selectedValue)
+                binding.heightIndicator.text = getString(R.string.height_value, selectedValue)
             }
 
         })
 
-        binding.weightPicker.setValuePickerListener(object : RulerValuePickerListener{
+        binding.weightPicker.setValuePickerListener(object : RulerValuePickerListener {
             override fun onValueChange(selectedValue: Int) {
             }
 
             override fun onIntermediateValueChange(selectedValue: Int) {
-                binding.weightIndicator.text = getString(R.string.weight_value,selectedValue)
+                binding.weightIndicator.text = getString(R.string.weight_value, selectedValue)
             }
 
         })
 
         binding.btnRegisterFinish.setOnClickListener {
             val queue = Volley.newRequestQueue(requireContext())
-            val gender = binding.genderSelect.checkedButtonId
+            val gender = when(binding.genderSelect.checkedButtonId){
+                R.id.btnMale -> 0
+                R.id.btnFemale -> 1
+                else -> 2
+            }
             val height = binding.heightPicker.currentValue
             val weight = binding.weightPicker.currentValue
-            //val dob = binding.textDob.editText?.text.toString()
+            val currentsdf = SimpleDateFormat.getDateInstance()
+            val serversdf = SimpleDateFormat("yyyy-MM-dd")
+            val dob = serversdf.format(currentsdf.parse(binding.textDob.editText?.text.toString()))
             val url = "http://10.0.2.2:5000/user/register/"
             val jsonBody = JSONObject()
-            jsonBody.put("gender","MALE")
-            jsonBody.put("weight",weight)
-            jsonBody.put("height",height)
-            //jsonBody.put("dob",dob)
+            jsonBody.put("gender", gender)
+            jsonBody.put("weight", weight)
+            jsonBody.put("height", height)
+            jsonBody.put("dob", dob)
 
             val registerRequest = object : JsonObjectRequest(Method.POST, url, jsonBody, {
-                Toast.makeText(requireContext(),"Registration Complete",Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Registration Complete", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_registerInformationFragment_to_mainActivity)
             },
                 {
-                    Log.e("REGISTER",it.toString())
+                    Log.e("REGISTER", it.toString())
                 }) {
                 @Throws(AuthFailureError::class)
                 override fun getHeaders(): Map<String, String> {
                     val headers = HashMap<String, String>()
                     headers["Content-Type"] = "application/json"
                     headers["Authorization"] = tokenCode
-                    Log.e("AUTH",headers["Authorization"]!!)
+                    Log.e("AUTH", headers["Authorization"]!!)
                     return headers
                 }
             }
